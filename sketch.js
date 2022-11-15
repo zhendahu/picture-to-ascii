318348880faefe964521ee88cfb65c077cc079b4
zhendahu <zhendahu@gmail.com>
2022-11-15 01:39:37 -0800

    introduce new assets, use p5 canvas rather than DOM

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
 const density = 'Ñ@#W$9876543210?!abc;:+=-,._ ';
+let image;
 
-let testimage;
-
-function preload(){
-  testimage = loadImage("assets/hoshitest.jpg");
+function preload() {
+  image = loadImage("assets/hoshitest50.jpg");
 }
 
-// We're using DOM instead of Canvas in order to actually load the text onto the website, as well as for easier adjustments to text size, font, etc.
-function setup(){
-  noCanvas();
+function setup() {
+  createCanvas(400, 400); 
+}
+
+function draw() {
   background(0);
+  
+  let w = width / image.width;
+  let h = height / image.height;
+  image.loadPixels();
+  for (let i = 0; i < image.width; i++) {
+    for (let j = 0; j < image.height; j++) {
+      const pixelIndex = (i + j * image.width) * 4;
+      const r = image.pixels[pixelIndex + 0];
+      const g = image.pixels[pixelIndex + 1];
+      const b = image.pixels[pixelIndex + 2];
+      const avg = (r + g + b) / 3;
+      
+      noStroke();
+      fill(255);
+      //square(i * w, j * h, w);
+      
+      const len = density.length;
+      const charIndex = floor(map(avg,0,255,len,0));
+      
+      textSize(w);
+      textAlign(CENTER, CENTER);
+      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
+      
+      
+    }
+  }
+  
+  
 }
